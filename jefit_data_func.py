ca1ba5820524f613337762b668d1a5ce2996a792
Bianca Ha <biancaha2001@gmail.com>
2024-09-17 22:19:50 -0400

    removed datetime func and embedded into clean_data

--- jefit_data_func.py
+++ jefit_data_func.py
@@ -1,10 +1,4 @@
 import pandas as pd
-
-# putting date into date obj
-def date_clean(x):
-    obj = pd.to_datetime(x)
-    return(obj)
-
 # function to create clean dataframe containing desired info from raw csvs 
 def clean_data(path,og_names,new_names):
     data = pd.read_csv(path,header=0)
@@ -16,7 +10,7 @@ def clean_data(path,og_names,new_names):
             df.loc[:,new_names[i]] = data.loc[:,og_names[i]]
         if 'TIMESTAMP' in og_names:
             new_name_date = new_names[og_names.index('TIMESTAMP')] # look for name used for timestamp in list
-            df[new_name_date] = df[new_name_date].apply(date_clean) 
+            df[new_name_date] = df[new_name_date].apply(lambda x: pd.to_datetime(x)) 
             df.sort_values(by=new_name_date) # sort df ascending by date
         return df
     else:
