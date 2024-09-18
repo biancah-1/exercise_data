6c33637b302ee3480c26415631583bcebc25f82b
Bianca Ha <biancaha2001@gmail.com>
2024-09-17 22:20:06 -0400

    started visualizing with dash

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from jefit_data_func import *
+from dash import Dash, dcc, html, Input, Output, callback
+import plotly.express as px
 
 # cleaning data for workouts
 w_path = 'data\workout_logs.csv'
@@ -16,7 +18,7 @@ ex_new = ['Date','Exercise_Logs','Exercise_Name']
 df_exercise = clean_data(ex_path,ex_og,ex_new)
 
 # filtering for July - Dec 2023, with 5 or more exercises in a session
-df_workouts_filt = df_workouts.loc[(df_workouts['Date'].dt.year==2023) & (df_workouts['Date'].dt.month>=7) & (df_workouts['Num_Exercises']>=5)]
+df_workouts_filt = df_workouts.loc[(df_workouts['Date'].dt.year==2023) & (df_workouts['Date'].dt.month>=5) & (df_workouts['Num_Exercises']>=5)]
 dates = df_workouts_filt['Date'].dt.date
 
 # selecting dates in exercise logs
@@ -25,11 +27,33 @@ df_exercise_filt = df_exercise[mask]
 
 # filtering to include 5 most common exercises
 ex_counts = df_exercise_filt['Exercise_Name'].value_counts()
-top5_ex_count = ex_counts.head(5).plot(kind='bar')
-#plt.show()
 top5_ex_name = ex_counts.head(5).index.tolist()
 ex_mask = (df_exercise_filt['Exercise_Name']).isin(top5_ex_name)
 df_exercise_top5 = df_exercise_filt[ex_mask]
 
 # adding calculated values 
 df_exercise_top5['Weight_Max'],df_exercise_top5['Volume_Sets'], df_exercise_top5['Volume_Total'] = zip(*df_exercise_top5['Exercise_Logs'].apply(weight_calcs))
+
+# removing time, keeping just date
+df_exercise_top5['Date'] = df_exercise_top5['Date'].dt.date 
+
+# Dash App for Displaying data
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Exercise Tracker'),
+    dcc.Dropdown(top5_ex_name, 'Barbell Bench Press',id='dropdown'),
+    dcc.Graph(id='graph-content')
+    ])
+
+@callback(
+    Output('graph-content','figure'),
+    Input('dropdown','value')
+)
+
+def update_graph(value):
+    vals = df_exercise_top5.loc[df_exercise_top5['Exercise_Name']==value]
+    return px.scatter(vals,x='Date',y='Weight_Max')
+
+if __name__ == '__main__':
+    app.run(debug=True)
